a9586eae9cdfd250dc19d31be3cde545b52d3e1d
Roger-JieW <80303423+Roger-JieW@users.noreply.github.com>
2022-04-04 15:33:12 +0930

    Prac4 5

--- Truckloads.cpp
+++ Truckloads.cpp
@@ -1,20 +1,17 @@
 #include <iostream>
-#include "TruckLoads.h"
+#include "Truckloads.h"
 
+using namespace std;
 
 int Truckloads::numTrucks(int numCrates, int loadSize){
-	if (numCrates==0){
-		return 0;
-	}
-	if (numCrates <= loadSize) {
-		return 1;
-	}
-	else
-	{
-		int first_half = 0;
-		int second_half = 0;
-		first_half = (numCrates + 1) / 2;
-		second_half = numCrates / 2;
-		return numTrucks(first_half, loadSize) + numTrucks(second_half, loadSize);
-	}
-}
+    if (numCrates <= loadSize) {
+        return 1;
+    }else{
+        if (numCrates % 2 == 0) {
+            return numTrucks(numCrates/2, loadSize) + numTrucks(numCrates/2, loadSize);
+        }
+        else{
+            return numTrucks((numCrates+1)/2, loadSize) + numTrucks(numCrates/2, loadSize);
+        }
+    }
+};
